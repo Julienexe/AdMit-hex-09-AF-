8dda88919b0f694226a260a3c3b38c1c1ab3ebcf
Julianexe <julianssonko@gmail.com>
2024-10-09 22:10:23 +0300

    add models

--- admit/admin.py
+++ admit/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import School, Applicant, Application, Payment
+from .models import School, Applicant, Application, Payment, Testimonial
 
 admin.site.register(School)
 admin.site.register(Applicant)
 admin.site.register(Application)
 admin.site.register(Payment)
+admin.site.register(Testimonial)
 
