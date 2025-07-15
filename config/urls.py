949669097bd20f4ffe9d1517cdf507838b87dc9d
Julienexe <julianssonko@gmail.com>
2025-07-15 21:44:42 +0300

    Add user URLs for registration and token management

--- config/urls.py
+++ config/urls.py
@@ -19,9 +19,13 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
 
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("",include("admit.urls")),
+    path("api/users/", include("users.urls")),
+
 ]
 
 if settings.DEBUG:
